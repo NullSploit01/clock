e42edd725f55f839a895c6d6b139f2ed6066b058
Harshal <harshaldharmik099@gmail.com>
2022-08-06 22:52:48 +0530

    finished timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import Button from "@mui/material/Button";
@@ -15,7 +15,9 @@ const Timer = () => {
   const [hour, updateHour] = useState("00");
   const [minute, updateMinute] = useState("00");
   const [second, updateSecond] = useState("00");
-
+  const [currentSecond, updateCurrentSecond] = useState(1);
+  const [currentMinute, updateCurrentMinute] = useState(1);
+  const [running, setRunning] = useState(false);
   const updateValues = (sign, threshold, value, updateFunction) => {
     if (sign === "+" && parseInt(value) === threshold) {
       updateFunction("00");
@@ -26,6 +28,59 @@ const Timer = () => {
     } else if (sign === "+" && parseInt(value) <= threshold) {
       updateFunction(pad(value % threshold, 2));
     }
+  };
+
+  const updateTimer = () => {
+    if (parseInt(second) > 0) {
+      updateSecond(pad(second - 2, 2));
+      updateCurrentSecond(currentSecond + 1);
+    } else if (parseInt(minute) > 0 && parseInt(second) === 0) {
+      updateSecond("59");
+      updateMinute(pad(minute - 2, 2));
+    } else if (
+      parseInt(hour) > 0 &&
+      parseInt(minute) === 0 &&
+      parseInt(second) === 0
+    ) {
+      updateSecond("59");
+      updateMinute("59");
+      updateHour(pad(hour - 2, 2));
+    } else if (currentSecond >= 59 && parseInt(minute) > 0) {
+      updateMinute(pad(minute - 2, 2));
+      updateCurrentMinute(currentMinute + 1);
+      updateCurrentSecond(1);
+      updateSecond("59");
+    } else if (currentMinute >= 59 && parseInt(hour) > 0) {
+      updateHour(pad(hour - 2, 2));
+      updateSecond("59");
+      updateMinute("59");
+      updateCurrentMinute(1);
+      updateCurrentSecond(1);
+    } else if (currentMinute >= 59 && parseInt(hour) > 0) {
+      updateMinute(pad(hour - 2, 2));
+    }
+  };
+
+  const checkDisableState = () => {
+    return parseInt(hour) + parseInt(minute) + parseInt(second) <= 0;
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (running) {
+        updateTimer();
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [second, running]);
+
+  const handleReset = () => {
+    updateHour("00");
+    updateMinute("00");
+    updateSecond("00");
+    updateCurrentMinute(1);
+    updateCurrentSecond(1);
+    setRunning(false);
   };
 
   const handleChange = (val, type) => {
@@ -100,12 +155,34 @@ const Timer = () => {
         </div>
       </div>
       <div className="values">{`${hour}:${minute}:${second}`}</div>
-      <div className="stop">
-        <Button size="large" variant="outlined">
-          START
-        </Button>
+      <div className="buttons">
+        <div className="button">
+          <Button
+            onClick={() => {
+              setRunning(!running);
+            }}
+            size="large"
+            variant="outlined"
+            disabled={checkDisableState()}
+          >
+            {!running ? "START" : "STOP"}
+          </Button>
+        </div>
+        <div className="button">
+          <Button
+            onClick={() => {
+              setRunning(!running);
+              handleReset();
+            }}
+            size="large"
+            variant="outlined"
+            disabled={checkDisableState()}
+            color="error"
+          >
+            RESET
+          </Button>
+        </div>
       </div>
-
       <Footer />
     </>
   );
